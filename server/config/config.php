27f6ed5dd0bbe271ed94caa3fd6b7db98e107b9b
immanuelredd <immanuelokputu94@gmail.com>
2021-07-06 13:48:55 +0100

    create server folder

---
+++ server/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+#DEFINE ALL PATHS LOCAL OR GLOBAL
+
+#local app root
+define('APPROOT', dirname(__DIR__));
+
+#urlroot
+define('URLROOT', 'http://pininteret.local/');
---
+++ server/init.php
@@ -0,0 +1,5 @@
+<?php
+
+#ALL FILES THAT REQUIRED TO HANDLE ALL FORMS
+include('config/config.php');
+include(APPROOT . '/formHandler/formHandler.php');
